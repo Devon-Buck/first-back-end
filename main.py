eadd7313ffe493375ef3d4056df7ecac368a4574
Devon Buck <devonmbuck1999@gmail.com>
2026-05-08 11:49:06 -0600

    Add idea model

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+from datetime import date
+
 from fastapi import FastAPI
+
+from models import Idea
 
 
 app = FastAPI()
 
+db: list[Idea] = []
+
 
 @app.get("/")
 async def hello() -> str:
-    return "Hello world!"
+    return "Hello from Devon!"
+
+
+@app.get("/ideas")
+async def get_ideas() -> list[Idea]:
+    return db
+
+
+@app.post("/ideas")
+async def create_ideas(title: str, content: str) -> None:
+    idea = Idea(title=title, content=content, created_at=date.today())
+    db.append(idea)
